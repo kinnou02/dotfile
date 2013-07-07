1068d5a7a5d40361786f0716edff78e13fb4ee45
kinou <kinou@rilynett.com>
2013-07-07 15:13:20 +0200

    update link.sh

--- etc/link.sh
+++ etc/link.sh
@@ -6,15 +6,15 @@ rm ~/.gvimrc
 rm -rfi ~/.vim
 
 
-ln -s ~/config/etc/vimrc ~/.vimrc
-ln -s ~/config/etc/gvimrc ~/.gvimrc
-ln -s ~/config/etc/vim/ ~/.vim
+ln -s ~/dotfile/etc/vimrc ~/.vimrc
+ln -s ~/dotfile/etc/gvimrc ~/.gvimrc
+ln -s ~/dotfile/etc/vim/ ~/.vim
 
 rm -rfi ~/.zsh
 rm ~/.zshrc
 
-ln -s ~/config/etc/zsh ~/.zsh
-ln -s ~/config/etc/zshrc ~/.zshrc
-ln -s ~/config/etc/Xdefaults ~/.Xdefaults
+ln -s ~/dotfile/etc/zsh ~/.zsh
+ln -s ~/dotfile/etc/zshrc ~/.zshrc
+ln -s ~/dotfile/etc/Xdefaults ~/.Xdefaults
 
 ln -s ~/config/bin ~/bin
